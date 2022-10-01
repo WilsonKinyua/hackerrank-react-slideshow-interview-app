192edc8e772e02ff25f2eb897ce8f440132bc7f4
wilsonkinyua <wilsonkinyuam@gmail.com>
2022-10-01 19:08:49 +0300

    test complete

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,22 +1,42 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-function Slides({slides}) {
-    const [slide,getSlide] = useState(slides)
-    console.log(slide)
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+function Slides({ slides }) {
+  const [slide, getSlide] = useState(0);
 
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          disabled={slide === 0}
+          onClick={() => getSlide(0)}
+        >
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          disabled={slide === 0}
+          onClick={() => getSlide(slide - 1)}
+        >
+          Prev
+        </button>
+        <button
+          data-testid="button-next"
+          className="small"
+          disabled={slide === slides.length - 1}
+          onClick={() => getSlide(slide + 1)}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slides[slide]["title"]}</h1>
+        <p data-testid="text">{slides[slide]["text"]}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
